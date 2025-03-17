d25b6210d29515124c4943cde61f7f77584973af
Samuel Freilich <sfreilich@google.com>
2025-03-17 14:59:38 -0400

    Add a Bazel build and a basic test

---
+++ libtess2_test.cc
@@ -0,0 +1,110 @@
+#include <cstdlib>
+#include <vector>
+
+#include "gtest/gtest.h"
+
+#include "tesselator.h"
+
+namespace {
+
+constexpr int kComponentCount = 2;
+constexpr int kNumTriangleVertices = 3;
+
+struct Vector2f {
+  float x;
+  float y;
+};
+
+void* HeapAlloc(void* user_data, unsigned int size) { return malloc(size); }
+void* HeapRealloc(void* user_data, void* ptr, unsigned int size) {
+  return realloc(ptr, size);
+}
+void HeapFree(void* user_data, void* ptr) { free(ptr); }
+
+// Adds a polygon with a hole to the tessellator.
+//
+// a = outer loop
+// b = inner loop
+// x = interior
+// o = exterior
+//
+// +aaaaaaaaaaaaaa+
+// a xx | xx | xx a
+// a xx | xx | xx a
+// a----+bbbb+----a
+// a xx b oo b xx a
+// a xx b oo b xx a
+// a----+bbbb+----a
+// a xx | xx | xx a
+// a xx | xx | xx a
+// +aaaaaaaaaaaaaa+
+//
+// This should tessellate to 8 triangles.
+void AddPolygonWithHole(TESStesselator* tess) {
+  std::vector<Vector2f> outer_loop = {
+      {0, 0},
+      {3, 0},
+      {3, 3},
+      {0, 3},
+  };
+  std::vector<Vector2f> inner_hole = {
+      {1, 1},
+      {2, 1},
+      {2, 2},
+      {1, 2},
+  };
+
+  tessSetOption(tess, TESS_REVERSE_CONTOURS, 0);
+  tessAddContour(tess, kComponentCount, outer_loop.data(),
+                 kComponentCount * sizeof(TESSreal), outer_loop.size());
+  tessSetOption(tess, TESS_REVERSE_CONTOURS, 1);
+  tessAddContour(tess, kComponentCount, inner_hole.data(),
+                 kComponentCount * sizeof(TESSreal), inner_hole.size());
+}
+
+// Tests that tessellation succeeds when the default allocator is used.
+TEST(Libtess2Test, DefaultAllocSuccess) {
+  TESStesselator* tess = tessNewTess(nullptr);
+  ASSERT_NE(tess, nullptr);
+
+  // Add the polygon and tessellate it.
+  AddPolygonWithHole(tess);
+  EXPECT_NE(tessTesselate(tess, TESS_WINDING_POSITIVE, TESS_POLYGONS,
+                          kNumTriangleVertices, kComponentCount, nullptr),
+            0);
+
+  // It should take 8 triangles to represent the polygon with the hole.
+  EXPECT_EQ(tessGetElementCount(tess), 8);
+
+  tessDeleteTess(tess);
+}
+
+TEST(Libtess2Test, CustomAllocSuccess) {
+  TESSalloc alloc = {
+      HeapAlloc,
+      HeapRealloc,
+      HeapFree,
+      /*user_data=*/nullptr,
+      /*meshEdgeBucketSize=*/512,
+      /*meshVertexBucketSize=*/512,
+      /*meshFaceBucketSize=*/256,
+      /*dictNodeBucketSize=*/512,
+      /*regionBucketSize=*/256,
+      /*extraVertices=*/0,
+  };
+  TESStesselator* tess = tessNewTess(&alloc);
+  ASSERT_NE(tess, nullptr);
+
+  // Add the polygon and tessellate it.
+  AddPolygonWithHole(tess);
+  EXPECT_NE(tessTesselate(tess, TESS_WINDING_POSITIVE, TESS_POLYGONS,
+                          kNumTriangleVertices, kComponentCount, nullptr),
+            0);
+
+  // It should take 8 triangles to represent the polygon with the hole.
+  EXPECT_EQ(tessGetElementCount(tess), 8);
+
+  tessDeleteTess(tess);
+}
+
+}  // namespace
